f861aa5aa8241a4989438eb81c1097714c48ba17
somka890 <somkiuks@gmail.com>
2025-07-30 20:51:56 +0300

    Pridėtas pirmasis puslapis
    Signed-off-by: somka890 <somkiuks@gmail.com>

--- mano_projektas/blogas/views.py
+++ mano_projektas/blogas/views.py
@@ -1,3 +1,11 @@
-
+from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+
+def pagrindinis(request):
+    kontekstas = {
+        'pavadinimas': 'Mano Puslapis',
+        'antraste': 'Sveiki atvykę!'
+    }
+    return render(request, 'pagrindinis.html', kontekstas)
